3e66d9c8b06e6ee897c9a60c94514aa2d976abb5
Amina EL KHATTABI <ael-khat@e1r1p11.42.fr>
2021-12-21 15:57:04 +0100

    pas davtc

--- get_next_line.c
+++ get_next_line.c
@@ -8,20 +8,17 @@ char    *get_next_line(int  fd)
 
     if  (fd == -1)
         ft_putstr_fd("Open file Error", 1);
-    printf("fd %d", fd);
-    printf("BUFF SIZE %d", BUFFER_SIZE);
-    ret_val = read(fd, tmp, BUFFER_SIZE);
-    printf("ret_value %d", ret_val);
-    /*while (ret_val >= 0)
+    //printf("fd %d", fd);
+    //printf("BUFF SIZE %d", BUFFER_SIZE);
+    while ((ret_val = read(fd, tmp, BUFFER_SIZE)))
     {
-        ret_val = read (fd, tmp, BUFFER_SIZE);
         tmp[ret_val] = '\0';
-        ft_putstr_fd(tmp, 1);
+        //ft_putstr_fd(tmp, 1);
+        printf("*************%s*********** \n", tmp);                   
     }
-    
-    */
+    //printf("ret_value %d", ret_val);
     return ("HELLO");
-}
+}               
 
 int main()
 {
@@ -30,7 +27,5 @@ int main()
     fd = open("42", O_RDWR | O_CREAT, S_IRWXU);
     if  (fd == -1)
         ft_putstr_fd("Open file Error", 1);
-    //printf("fd dans le main %d", fd);
-    ft_putstr_fd("Hey you", fd);
     get_next_line(fd);
 }
